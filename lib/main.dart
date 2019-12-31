dfd69497bfd0ff0fc9b8f4e5088378fdcaafab00
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-12-31 02:46:00 +0200

    add initial layout (question, two buttons)

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ void main() => runApp(QuizzApp());
 class QuizzApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: QuizzPage()
-    );
+    return MaterialApp(home: QuizzPage());
   }
 }
 
@@ -16,8 +14,60 @@ class QuizzPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    'This will be the place for the question',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 30.0),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                color: Colors.green,
+                onPressed: () {
+                  print('Pressed true');
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                color: Colors.red,
+                onPressed: () {
+                  print('pressed false');
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
-
-
